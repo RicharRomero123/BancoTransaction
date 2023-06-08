d36fb884d069703a477ef1a9d992e91a8b38d2b5
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-06-07 20:00:25 -0500

    Update repostiroy query

--- src/main/java/com/ejercicio3/Banco/repository/TransactionRepository.java
+++ src/main/java/com/ejercicio3/Banco/repository/TransactionRepository.java
@@ -16,6 +16,6 @@ public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
     boolean existsByCreateDateRange(LocalDate First_createDate, LocalDate Last_createDate);
 
-    @Query("SELECT t FROM Transactions t between t.first_date_created and t.second_date_created")
-    List<Transaction> filterByCreateDateRange(@Param("first_date_created") LocalDate First_createDate, @Param("second_date_created") LocalDate Last_createDate);
+    @Query("SELECT t FROM Transaction t WHERE t.createDate BETWEEN :first_date_created AND :second_date_created")
+    List<Transaction> filterByCreateDateRange(@Param("first_date_created") LocalDate first_date_created, @Param("second_date_created") LocalDate second_date_created);
 }
